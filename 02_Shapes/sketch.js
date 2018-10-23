548ce549f50579a9388e2f3b124a5f76461353a0
(s) Renato Vidal Ballon <rvidalballon@pm7-0100.uopnet.plymouth.ac.uk>
2018-10-23 17:41:01 +0100

    update

---
+++ 02_Shapes/sketch.js
@@ -0,0 +1,33 @@
+
+//Initialization function
+function setup() {
+  //Set the size of rendering window - pixels
+  createCanvas(500, 500);
+
+  background(000);
+
+// set colors
+  fill(255, 0, 0);
+  stroke(127,63,120);
+  strokeWeight(10);
+
+  //an ellipse
+ line( 50,50,450,450)
+  ellipse( 100,100,180,180);
+
+  line( 450,50,50,450);
+    ellipse( 400,400,180,180);
+
+     line( 50,450,320,450);
+      ellipse( 100,400,180,180);
+
+      line( 450,320,450,50);
+        ellipse( 400,100,180,180);
+
+
+}
+
+//Rendering function
+function draw() {
+
+}
