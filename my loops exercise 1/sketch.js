4b10424d195e9914b34cb6c89f3e58345c16709d
thedrifter426 <alabhama426@hotmail.com>
2018-11-07 13:28:05 +0000

    My Loops Exercise 1

--- my loops exercise 1/sketch.js
+++ my loops exercise 1/sketch.js
@@ -3,3 +3,8 @@ for(n = 1; n <= 12; n++)
   total= n * 10;
   window.alert("total is equal to" + total);
   }
+
+/* this FOR loop sets the initial value of the n loop variable to 1
+and execute the two statements between the curly braces {}, if n is
+less than or equal to 12, and will increment n by 1 after each pass
+through the loop */
