cc973e5b760a852fd0dad3068fb543d0b0c09afe
thedrifter426 <alabhama426@hotmail.com>
2018-11-10 22:46:32 +0000

    my var exercise 3

---
+++ my var exercise 3/sketch.js
@@ -0,0 +1,9 @@
+var inches = prompt("input inches Here", 5);
+var cm = 0;
+cm = inches/0.39370;
+   alert(inches + " inches equals " + inches/0.39370 + " cm");
+
+/* in this exercise I build a metric measure converter,
+converting old imperial measures of length in inches to centimetres
+here the number of inches is entered via the window prompt() method,
+and the calculation displays the result */
