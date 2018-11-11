fd884f7b7710d608400b9ab71a98dafe2952f478
thedrifter426 <alabhama426@hotmail.com>
2018-11-11 01:18:12 +0000

    update

--- my var exercise 2/sketch.js
+++ my var exercise 2/sketch.js
@@ -1,2 +1,8 @@
 var first = window.prompt("Please enter your name:");
 document.write("Welcome to University of Plymouth, " + first + ".");
+
+/* here I'm declaring a variable called "first",
+which will be populated by the value user enter into the window prompt box
+next, I'm using the document.write method to display a welcome string,
+which includes a comma and a space at the end, plus the value in the "first"
+variable plus a full stop. */
