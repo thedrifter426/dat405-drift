9430a2ddceaca9e56a20ce41ba9b2d96dc64c9bc
thedrifter426 <alabhama426@hotmail.com>
2018-11-11 17:22:08 +0000

    update

--- function exercise2/sketch.js
+++ function exercise2/sketch.js
@@ -6,4 +6,15 @@ function getData()
    var circumference = 2 * PI * radius;
    var area = PI * (radius * radius);
    output.innerHTML = "Radius: " + radius + "\nCircumference: " + circumference + "\nArea: " + area;
-} 
+}
+
+/* here I'm using the function getData() to Calculate
+the area and circumference of a circle, obtaining
+the radius from a form field and using the onSubmit event
+to pass the radius to the function */
+
+/* area of a circle is calculated as follow:
+area = 3.14159 * (radius * radius)
+
+circumference of a circle is calculated as follow:
+circumference = 2 * 3.14159 * radius  */
