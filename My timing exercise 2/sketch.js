4495d84f013d31ecf3e472745c68340fd8b119fe
thedrifter426 <alabhama426@hotmail.com>
2018-11-14 21:45:25 +0000

    My timing exercise 2

---
+++ My timing exercise 2/sketch.js
@@ -0,0 +1,22 @@
+function display() {
+
+	  setInterval(wave,3000);
+	  var n = 1;
+	  n = n++;
+}
+	  function wave() {
+
+        	 alert("this is alert number" + " n ");
+}
+     function stop() {
+	 alert(" stop ");
+   
+
+   /* in this exercise i want to display an alert box every three seconds,
+   with the text "this is alert number", followed by the number of times
+   the alert has been run ( I'm using the variable n as a counter).*/
+
+   /* also i want to provide a button which, when clicked, will
+   stop the interval timer */
+
+}
