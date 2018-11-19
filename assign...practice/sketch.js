b4c15c748c0c75a207e86c9758d267841bad62d8
thedrifter426 <alabhama426@hotmail.com>
2018-11-19 19:54:12 +0000

    update

--- assign...practice/sketch.js
+++ assign...practice/sketch.js
@@ -1,11 +1,11 @@
 function setup() {
-  createCanvas(720, 400);
+  createCanvas(841, 594);
   background(0);
   noStroke();
 }
 
 function draw() {
   fill(color(random(255),random(150),random(80), 1));
-  bezier(random(500, 500), random(100, 500), 10, 10, 40, 40, 50, -9000);
-  bezier(random(100, 500), random(100, 300), 10, 10, 90, 90, 50, 20);
+  bezier(random(100, 500), random(100, 500), 100, 0, 40, 400, 5000, 9000);
+  bezier(random(100, 800), random(100, 300), 0, 0, 400, 400, 500, 0);
 }
