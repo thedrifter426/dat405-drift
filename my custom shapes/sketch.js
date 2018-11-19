1af541a686f94bcee1251bb867fac2ee069a43d3
thedrifter426 <alabhama426@hotmail.com>
2018-11-19 19:55:10 +0000

    update

--- my custom shapes/sketch.js
+++ my custom shapes/sketch.js
@@ -1,7 +1,7 @@
 let sizeRandom = 100;
 let xRandom;
 let yRandom;
-
+let state;
 function setup() {
 
   createCanvas(1020, 500);
@@ -20,24 +20,36 @@ function draw()  {
 
 function customShape(x, y, size) {
 
-  stroke(0, 0, 478);
-  strokeWeight(50);
-  fill(random(255), random(255), random(255));
-  ellipse(x, y, size, size);
+  if (state == 1) {
+    stroke(0, 0, 478);
+    strokeWeight(5);
+    fill(0,0,0);
+    fill(random(255), random(255), random(255));
+    ellipse(x, y, size, size);
+}
+    else if (state == 2) {
+     stroke(478, 0, 0);
+     strokeWeight(5);
+     fill(0,0,0);
+     fill(random(255), random(255), random(255));
+     rect(x, y, size, size);
+  }
 
-
-  stroke(478, 0, 0);
-  strokeWeight(50);
+  else if (state == 3) {
+  stroke(0, 0, 178);
+  strokeWeight(3);
+  fill(0,0,0);
   fill(random(255), random(255), random(255));
-  rect(x, y, size, size);
+  triangle(x, y,size, size);
+}
 }
 
-   function keyPressed() {
+function keyPressed() {
+  if (key == '1') state = 1;
+  if (key == '2') state = 2;
+  if (key == '3') state = 3;
+}
 
-      if (key == "A") displayEllipse;
-      if (key == "S") displayRect;
-      if (key == "3");
-   }
   /* stroke(0, 0, 178);
    strokeWeight(3);
    fill(0, 0, 0);
